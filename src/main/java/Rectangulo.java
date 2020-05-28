4c0264c81968e156badafae50f26a82fabd311d8
Vanesa <vanesa.fernandez.alum@gmail.com>
2020-05-28 19:34:02 +0200

    "Creacion de la clase 'Rectangulo' y del metodo 'calcularArea'."

---
+++ src/main/java/Rectangulo.java
@@ -0,0 +1,15 @@
+
+public class Rectangulo {
+    
+    private double lado1;
+    private double lado2;
+    
+    public Rectangulo(double l1 , double l2){
+        lado1 = l1;
+        lado2 = l2;
+    }
+    
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+}
